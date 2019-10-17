7cf4eefff79b48bee496c76596a2fac6fdf3b397
Runjuu <huang@runjuu.com>
2019-10-17 13:30:25 +0800

    feat: remove @themes/asset

--- packages/asset/README.md
+++
@@ -1,3 +0,0 @@
-# @themes/asset
-
-Work in process.
--- packages/asset/src/get-asset-scheme.ts
+++
@@ -1,9 +0,0 @@
-import { Asset, AssetScheme, AssetThemesType } from './types'
-
-export function getAssetScheme<T extends AssetThemesType, S>(
-  asset: Asset<T, S>,
-  specificTheme: T = asset.defaultTheme,
-): AssetScheme<Asset<T, S>> {
-  const scheme = asset.schemes[specificTheme]
-  return typeof scheme === 'function' ? scheme() : scheme
-}
