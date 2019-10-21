fff181479e9e0111d74a73b7d92fa43015e0d266
Runjuu <huang@runjuu.com>
2019-10-21 14:14:17 +0800

    feat(site): use `use-dark-mode` hooks to switch color theme

--- packages/site/src/index.tsx
+++ packages/site/src/index.tsx
@@ -1,29 +1,13 @@
 import * as React from 'react'
-import { useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import { useDarkMode } from '@themes/use-dark-mode'
 
 import { ColorsProvider, ColorsTheme } from './colors'
 import { Home } from './pages'
 import { GlobalStyle } from './global.styled'
 
 const App = () => {
-  const [colorTheme, updateColorTheme] = React.useState(
-    matchMedia('(prefers-color-scheme: dark)').matches ? ColorsTheme.dark : ColorsTheme.light,
-  )
-
-  useEffect(() => {
-    const onPrefersColorSchemeChanged = (event: MediaQueryListEvent) => {
-      updateColorTheme(event.matches ? ColorsTheme.dark : ColorsTheme.light)
-    }
-
-    const mediaQueryList = matchMedia('(prefers-color-scheme: dark)')
-
-    mediaQueryList.addListener(onPrefersColorSchemeChanged)
-
-    return () => {
-      mediaQueryList.removeListener(onPrefersColorSchemeChanged)
-    }
-  }, [updateColorTheme])
+  const colorTheme = useDarkMode(ColorsTheme)
 
   return (
     <ColorsProvider value={colorTheme}>
