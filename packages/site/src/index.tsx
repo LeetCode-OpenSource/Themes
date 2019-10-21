903d50b7d9a76e38dd1b15ad5aef2a848b9d6645
Runjuu <huang@runjuu.com>
2019-10-21 23:50:20 +0800

    feat(site): replace `useDarkMode` with `useMatchMedia`

--- packages/site/src/index.tsx
+++ packages/site/src/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import { useDarkMode } from '@themes/use-dark-mode'
+import { useMatchMedia } from '@themes/react'
 
 import { ColorsProvider, ColorsTheme } from './colors'
 import { Home } from './pages'
 import { GlobalStyle } from './global.styled'
 
 const App = () => {
-  const colorTheme = useDarkMode(ColorsTheme)
+  const colorTheme = useMatchMedia('(prefers-color-scheme: dark)')
+    ? ColorsTheme.dark
+    : ColorsTheme.light
 
   return (
     <ColorsProvider value={colorTheme}>
