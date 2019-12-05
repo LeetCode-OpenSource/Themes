525f1bc46ff6a6db75953121d31003a2bdc28ffa
Runjuu <huang@runjuu.com>
2019-12-05 12:36:07 +0800

    feat(scheme): update `OverwriteConfig` type

--- packages/scheme/src/types.ts
+++ packages/scheme/src/types.ts
@@ -11,8 +11,16 @@ export type OverwriteScheme<SchemeKey extends SchemeKeyType, Scheme> =
   | Partial<Scheme>
   | ((currentScheme: Scheme, currentSchemeKey: SchemeKey) => Partial<Scheme>)
 
-export interface OverwriteConfig<SchemeKey extends SchemeKeyType, Scheme> {
+type DefaultOverwriteConfig<SchemeKey extends SchemeKeyType, Scheme> = {
   identify: typeof OverwriteSymbol
   schemeKey?: SchemeKey
   overwriteScheme: OverwriteScheme<SchemeKey, Scheme>
 }
+
+export type OverwriteConfig<SchemeKey extends SchemeKeyType, Scheme> = SchemeKey extends infer SK
+  ? SK extends string
+    ? Scheme extends infer S
+      ? DefaultOverwriteConfig<SK, S>
+      : DefaultOverwriteConfig<SK, Scheme>
+    : DefaultOverwriteConfig<SchemeKey, Scheme>
+  : DefaultOverwriteConfig<SchemeKey, Scheme>
